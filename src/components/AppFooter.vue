2b541f510f6907b19615e1157b053a0d2507b4fc
PaoloNapoli97 <paolonapoli@live.it>
2022-12-02 02:01:05 +0100

    Fix: super error video

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,7 +5,18 @@ export default {
 </script>
 
 <template>
-  <main></main>
+  <main>
+    <iframe
+      width="420"
+      height="345"
+      src="https://www.youtube.com/embed/oEOJZUsYcU0"
+    >
+    </iframe>
+  </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+main {
+  text-align: center;
+}
+</style>
